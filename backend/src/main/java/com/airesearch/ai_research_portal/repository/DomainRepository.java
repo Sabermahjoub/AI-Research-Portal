b92e464a1b8d10d8ad8273860f74a832ed2242bb
Moataz Khabbouchi <mootazkhabbouchi8@gmail.com>
2025-03-01 12:46:04 +0100

    implemented domain code (no DTO)

---
+++ backend/src/main/java/com/airesearch/ai_research_portal/repository/DomainRepository.java
@@ -0,0 +1,7 @@
+package com.airesearch.ai_research_portal.repository;
+
+import com.airesearch.ai_research_portal.model.Domain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DomainRepository extends JpaRepository<Domain, Long> {
+}
