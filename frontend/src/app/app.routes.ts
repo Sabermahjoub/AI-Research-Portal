6315a277f73b23c9daeca5bf530470e9f21f3445
Saber Mahjoub <saber.mahjoub003@gmail.com>
2025-04-27 14:02:12 +0100

    fixing the routes conflict in branch

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,23 +1,12 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ListPublicationsComponent } from './components/list-publications/list-publications.component';
-import { MyAccountComponent } from './components/my-account/my-account.component';
-import { MyPublicationsComponent } from './components/my-publications/my-publications.component';
-
-import { LoginComponent } from './components/auth/login/login.component';
-import { SignupComponent } from './components/auth/signup/signup.component';
 import { AuthGuard } from './guards/auth.guard';
+import { PendingPublicationsComponent } from './components/pending-publications/pending-publications.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component'; 
 
 export const routes: Routes = [
-    // {   path : '',
-    //     component : HomeComponent,
-    //     children: [
-    //         { path: 'publications', component: ListPublicationsComponent }
-    //     ]
-    // },
-    // { path : 'publications', component : ListPublicationsComponent}
+    { path: '', redirectTo: 'CoreAI', pathMatch: 'full' },
 
     // Login & Sign Up Routes (public)
+    { path: 'CoreAI', component: LandingPageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
 
@@ -28,6 +17,7 @@ export const routes: Routes = [
       children: [
         { path: '', redirectTo: 'list', pathMatch: 'full' },
         { path: 'list', component: ListPublicationsComponent }, // Public route
+        { path: 'pending-publications', component: PendingPublicationsComponent }, // Public route
         {
           path: 'my-account',
           component: MyAccountComponent,
@@ -43,5 +33,4 @@ export const routes: Routes = [
 
     // Redirect any unknown paths to login
     { path: '**', redirectTo: '/login' }
-
 ];
